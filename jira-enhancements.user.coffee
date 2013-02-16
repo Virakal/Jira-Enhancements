7b8a4968c7c829d9b79d162ee482c525664a59c7
Jonathan Goodger <jonno.is@gmail.com>
2013-02-16 10:56:38 +0000

    Fix double click

--- jira-enhancements.user.coffee
+++ jira-enhancements.user.coffee
@@ -18,8 +18,13 @@ main = ($) ->
 
     clearImmediate = window.clearTimeout
 
+    insertDependencies = ->
+        # http://www.trendskitchens.co.nz/jquery/contextmenu/
+        `(function($){var menu,shadow,trigger,content,hash,currentTarget;var defaults={menuStyle:{listStyle:'none',padding:'1px',margin:'0px',backgroundColor:'#fff',border:'1px solid #999',width:'100px'},itemStyle:{margin:'0px',color:'#000',display:'block',cursor:'default',padding:'3px',border:'1px solid #fff',backgroundColor:'transparent'},itemHoverStyle:{border:'1px solid #0a246a',backgroundColor:'#b6bdd2'},eventPosX:'pageX',eventPosY:'pageY',shadow:true,onContextMenu:null,onShowMenu:null};$.fn.contextMenu=function(id,options){if(!menu){menu=$('<div id="jqContextMenu"></div>').hide().css({position:'absolute',zIndex:'500'}).appendTo('body').bind('click',function(e){e.stopPropagation()})}if(!shadow){shadow=$('<div></div>').css({backgroundColor:'#000',position:'absolute',opacity:0.2,zIndex:499}).appendTo('body').hide()}hash=hash||[];hash.push({id:id,menuStyle:$.extend({},defaults.menuStyle,options.menuStyle||{}),itemStyle:$.extend({},defaults.itemStyle,options.itemStyle||{}),itemHoverStyle:$.extend({},defaults.itemHoverStyle,options.itemHoverStyle||{}),bindings:options.bindings||{},shadow:options.shadow||options.shadow===false?options.shadow:defaults.shadow,onContextMenu:options.onContextMenu||defaults.onContextMenu,onShowMenu:options.onShowMenu||defaults.onShowMenu,eventPosX:options.eventPosX||defaults.eventPosX,eventPosY:options.eventPosY||defaults.eventPosY});var index=hash.length-1;$(this).bind('contextmenu',function(e){var bShowContext=(!!hash[index].onContextMenu)?hash[index].onContextMenu(e):true;if(bShowContext)display(index,this,e,options);return false});return this};function display(index,trigger,e,options){var cur=hash[index];content=$('#'+cur.id).find('ul:first').clone(true);content.css(cur.menuStyle).find('li').css(cur.itemStyle).hover(function(){$(this).css(cur.itemHoverStyle)},function(){$(this).css(cur.itemStyle)}).find('img').css({verticalAlign:'middle',paddingRight:'2px'});menu.html(content);if(!!cur.onShowMenu)menu=cur.onShowMenu(e,menu);$.each(cur.bindings,function(id,func){$('#'+id,menu).bind('click',function(e){hide();func(trigger,currentTarget)})});menu.css({'left':e[cur.eventPosX],'top':e[cur.eventPosY]}).show();if(cur.shadow)shadow.css({width:menu.width(),height:menu.height(),left:e.pageX+2,top:e.pageY+2}).show();$(document).one('click',hide)}function hide(){menu.hide();shadow.hide()}$.contextMenu={defaults:function(userDefaults){$.each(userDefaults,function(i,val){if(typeof val=='object'&&defaults[i]){$.extend(defaults[i],val)}else defaults[i]=val})}}})($)`
+        
+
     isBlocked = (link) ->
-        !!link.type.outward is "blocks" and link.outwardIssue?.fields.status.name isnt "Done"
+        !!(link.type.outward is "blocks" and link.outwardIssue?.fields.status.name isnt "Done")
 
     setBlocked = (card, value = true) ->
         flag = $(card).find '.blocked-flag'
@@ -28,13 +33,9 @@ main = ($) ->
 
     
     buildMenu = (id, items) ->
-        # http://www.trendskitchens.co.nz/jquery/contextmenu/
-        `(function($){var menu,shadow,trigger,content,hash,currentTarget;var defaults={menuStyle:{listStyle:'none',padding:'1px',margin:'0px',backgroundColor:'#fff',border:'1px solid #999',width:'100px'},itemStyle:{margin:'0px',color:'#000',display:'block',cursor:'default',padding:'3px',border:'1px solid #fff',backgroundColor:'transparent'},itemHoverStyle:{border:'1px solid #0a246a',backgroundColor:'#b6bdd2'},eventPosX:'pageX',eventPosY:'pageY',shadow:true,onContextMenu:null,onShowMenu:null};$.fn.contextMenu=function(id,options){if(!menu){menu=$('<div id="jqContextMenu"></div>').hide().css({position:'absolute',zIndex:'500'}).appendTo('body').bind('click',function(e){e.stopPropagation()})}if(!shadow){shadow=$('<div></div>').css({backgroundColor:'#000',position:'absolute',opacity:0.2,zIndex:499}).appendTo('body').hide()}hash=hash||[];hash.push({id:id,menuStyle:$.extend({},defaults.menuStyle,options.menuStyle||{}),itemStyle:$.extend({},defaults.itemStyle,options.itemStyle||{}),itemHoverStyle:$.extend({},defaults.itemHoverStyle,options.itemHoverStyle||{}),bindings:options.bindings||{},shadow:options.shadow||options.shadow===false?options.shadow:defaults.shadow,onContextMenu:options.onContextMenu||defaults.onContextMenu,onShowMenu:options.onShowMenu||defaults.onShowMenu,eventPosX:options.eventPosX||defaults.eventPosX,eventPosY:options.eventPosY||defaults.eventPosY});var index=hash.length-1;$(this).bind('contextmenu',function(e){var bShowContext=(!!hash[index].onContextMenu)?hash[index].onContextMenu(e):true;if(bShowContext)display(index,this,e,options);return false});return this};function display(index,trigger,e,options){var cur=hash[index];content=$('#'+cur.id).find('ul:first').clone(true);content.css(cur.menuStyle).find('li').css(cur.itemStyle).hover(function(){$(this).css(cur.itemHoverStyle)},function(){$(this).css(cur.itemStyle)}).find('img').css({verticalAlign:'middle',paddingRight:'2px'});menu.html(content);if(!!cur.onShowMenu)menu=cur.onShowMenu(e,menu);$.each(cur.bindings,function(id,func){$('#'+id,menu).bind('click',function(e){hide();func(trigger,currentTarget)})});menu.css({'left':e[cur.eventPosX],'top':e[cur.eventPosY]}).show();if(cur.shadow)shadow.css({width:menu.width(),height:menu.height(),left:e.pageX+2,top:e.pageY+2}).show();$(document).one('click',hide)}function hide(){menu.hide();shadow.hide()}$.contextMenu={defaults:function(userDefaults){$.each(userDefaults,function(i,val){if(typeof val=='object'&&defaults[i]){$.extend(defaults[i],val)}else defaults[i]=val})}}})($)`
-        
-        elem = $('<div>').attr(
+        elem = $('<div>').attr
             'class': 'contextMenu'
             'id': id
-        )
 
         elem.hide()
 
@@ -136,7 +137,9 @@ main = ($) ->
                         error: (xhr, status, error) ->
                             console.debug "Jon: Error fetching data for issue #{$(card).data()}"
 
+    # MAIN
 
+    insertDependencies()
     buildMenu 'issueContextMenu',
         'view-story':
             name: 'View Story...'
@@ -165,12 +168,20 @@ main = ($) ->
     $('#fancybox-outer').on 'dblclick', '#fancybox-img', (e) ->
         window.open $(this).attr 'src'
 
-    $('#ghx-work').on 'dblclick', '.ghx-issue', (e) ->
-        $(e.currentTarget).find('.js-detailview')[0].click()
-
     $(document).bind "DOMNodeInserted", (e) ->
         # span#js-pool-end is added when 'work' is reloaded
-        initIssueCards() if $(e.target).attr('id') is 'js-pool-end'
+        target = $ e.target
+        initIssueCards() if target.attr('id') is 'js-pool-end'
+        target.attr 'target', 'blank' if target.attr('class') is 'external-link'
+
+        return true
+
+    # Ugly hack to ensure the handler gets set
+    setTimeout((-> $('#ghx-work').on 'dblclick', '.ghx-issue', (e) ->
+        console.log "Clicked on ", $(e.currentTarget).find('.js-detailview')[0]
+        $(e.currentTarget).find('.js-detailview')[0].click()
+    ), 100)
+
 
 # Inject the JS in as a script tag to gain access to jQuery from AJS
 script = document.createElement 'script'
--- jira-enhancements.user.js
+++ jira-enhancements.user.js
@@ -12,16 +12,19 @@
 var main, script;
 
 main = function($) {
-  var API_URL, bindings, buildMenu, clearImmediate, decorateCard, initIssueCards, isBlocked, setBlocked, setImmediate, _ref;
+  var API_URL, bindings, buildMenu, clearImmediate, decorateCard, initIssueCards, insertDependencies, isBlocked, setBlocked, setImmediate, _ref;
   API_URL = '/rest/api/2/';
   bindings = {};
   setImmediate = (_ref = window.setImmediate) != null ? _ref : function(func, args) {
     return window.setTimeout(func, 0, args);
   };
   clearImmediate = window.clearTimeout;
+  insertDependencies = function() {
+    return (function($){var menu,shadow,trigger,content,hash,currentTarget;var defaults={menuStyle:{listStyle:'none',padding:'1px',margin:'0px',backgroundColor:'#fff',border:'1px solid #999',width:'100px'},itemStyle:{margin:'0px',color:'#000',display:'block',cursor:'default',padding:'3px',border:'1px solid #fff',backgroundColor:'transparent'},itemHoverStyle:{border:'1px solid #0a246a',backgroundColor:'#b6bdd2'},eventPosX:'pageX',eventPosY:'pageY',shadow:true,onContextMenu:null,onShowMenu:null};$.fn.contextMenu=function(id,options){if(!menu){menu=$('<div id="jqContextMenu"></div>').hide().css({position:'absolute',zIndex:'500'}).appendTo('body').bind('click',function(e){e.stopPropagation()})}if(!shadow){shadow=$('<div></div>').css({backgroundColor:'#000',position:'absolute',opacity:0.2,zIndex:499}).appendTo('body').hide()}hash=hash||[];hash.push({id:id,menuStyle:$.extend({},defaults.menuStyle,options.menuStyle||{}),itemStyle:$.extend({},defaults.itemStyle,options.itemStyle||{}),itemHoverStyle:$.extend({},defaults.itemHoverStyle,options.itemHoverStyle||{}),bindings:options.bindings||{},shadow:options.shadow||options.shadow===false?options.shadow:defaults.shadow,onContextMenu:options.onContextMenu||defaults.onContextMenu,onShowMenu:options.onShowMenu||defaults.onShowMenu,eventPosX:options.eventPosX||defaults.eventPosX,eventPosY:options.eventPosY||defaults.eventPosY});var index=hash.length-1;$(this).bind('contextmenu',function(e){var bShowContext=(!!hash[index].onContextMenu)?hash[index].onContextMenu(e):true;if(bShowContext)display(index,this,e,options);return false});return this};function display(index,trigger,e,options){var cur=hash[index];content=$('#'+cur.id).find('ul:first').clone(true);content.css(cur.menuStyle).find('li').css(cur.itemStyle).hover(function(){$(this).css(cur.itemHoverStyle)},function(){$(this).css(cur.itemStyle)}).find('img').css({verticalAlign:'middle',paddingRight:'2px'});menu.html(content);if(!!cur.onShowMenu)menu=cur.onShowMenu(e,menu);$.each(cur.bindings,function(id,func){$('#'+id,menu).bind('click',function(e){hide();func(trigger,currentTarget)})});menu.css({'left':e[cur.eventPosX],'top':e[cur.eventPosY]}).show();if(cur.shadow)shadow.css({width:menu.width(),height:menu.height(),left:e.pageX+2,top:e.pageY+2}).show();$(document).one('click',hide)}function hide(){menu.hide();shadow.hide()}$.contextMenu={defaults:function(userDefaults){$.each(userDefaults,function(i,val){if(typeof val=='object'&&defaults[i]){$.extend(defaults[i],val)}else defaults[i]=val})}}})($);
+  };
   isBlocked = function(link) {
     var _ref1;
-    return !!link.type.outward === "blocks" && ((_ref1 = link.outwardIssue) != null ? _ref1.fields.status.name : void 0) !== "Done";
+    return !!(link.type.outward === "blocks" && ((_ref1 = link.outwardIssue) != null ? _ref1.fields.status.name : void 0) !== "Done");
   };
   setBlocked = function(card, value) {
     var flag;
@@ -36,8 +39,6 @@ main = function($) {
     }
   };
   buildMenu = function(id, items) {
-    (function($){var menu,shadow,trigger,content,hash,currentTarget;var defaults={menuStyle:{listStyle:'none',padding:'1px',margin:'0px',backgroundColor:'#fff',border:'1px solid #999',width:'100px'},itemStyle:{margin:'0px',color:'#000',display:'block',cursor:'default',padding:'3px',border:'1px solid #fff',backgroundColor:'transparent'},itemHoverStyle:{border:'1px solid #0a246a',backgroundColor:'#b6bdd2'},eventPosX:'pageX',eventPosY:'pageY',shadow:true,onContextMenu:null,onShowMenu:null};$.fn.contextMenu=function(id,options){if(!menu){menu=$('<div id="jqContextMenu"></div>').hide().css({position:'absolute',zIndex:'500'}).appendTo('body').bind('click',function(e){e.stopPropagation()})}if(!shadow){shadow=$('<div></div>').css({backgroundColor:'#000',position:'absolute',opacity:0.2,zIndex:499}).appendTo('body').hide()}hash=hash||[];hash.push({id:id,menuStyle:$.extend({},defaults.menuStyle,options.menuStyle||{}),itemStyle:$.extend({},defaults.itemStyle,options.itemStyle||{}),itemHoverStyle:$.extend({},defaults.itemHoverStyle,options.itemHoverStyle||{}),bindings:options.bindings||{},shadow:options.shadow||options.shadow===false?options.shadow:defaults.shadow,onContextMenu:options.onContextMenu||defaults.onContextMenu,onShowMenu:options.onShowMenu||defaults.onShowMenu,eventPosX:options.eventPosX||defaults.eventPosX,eventPosY:options.eventPosY||defaults.eventPosY});var index=hash.length-1;$(this).bind('contextmenu',function(e){var bShowContext=(!!hash[index].onContextMenu)?hash[index].onContextMenu(e):true;if(bShowContext)display(index,this,e,options);return false});return this};function display(index,trigger,e,options){var cur=hash[index];content=$('#'+cur.id).find('ul:first').clone(true);content.css(cur.menuStyle).find('li').css(cur.itemStyle).hover(function(){$(this).css(cur.itemHoverStyle)},function(){$(this).css(cur.itemStyle)}).find('img').css({verticalAlign:'middle',paddingRight:'2px'});menu.html(content);if(!!cur.onShowMenu)menu=cur.onShowMenu(e,menu);$.each(cur.bindings,function(id,func){$('#'+id,menu).bind('click',function(e){hide();func(trigger,currentTarget)})});menu.css({'left':e[cur.eventPosX],'top':e[cur.eventPosY]}).show();if(cur.shadow)shadow.css({width:menu.width(),height:menu.height(),left:e.pageX+2,top:e.pageY+2}).show();$(document).one('click',hide)}function hide(){menu.hide();shadow.hide()}$.contextMenu={defaults:function(userDefaults){$.each(userDefaults,function(i,val){if(typeof val=='object'&&defaults[i]){$.extend(defaults[i],val)}else defaults[i]=val})}}})($);
-
     var config, elem, ul;
     elem = $('<div>').attr({
       'class': 'contextMenu',
@@ -143,6 +144,7 @@ main = function($) {
     }
     return _results;
   };
+  insertDependencies();
   buildMenu('issueContextMenu', {
     'view-story': {
       name: 'View Story...',
@@ -171,14 +173,23 @@ main = function($) {
   $('#fancybox-outer').on('dblclick', '#fancybox-img', function(e) {
     return window.open($(this).attr('src'));
   });
-  $('#ghx-work').on('dblclick', '.ghx-issue', function(e) {
-    return $(e.currentTarget).find('.js-detailview')[0].click();
-  });
-  return $(document).bind("DOMNodeInserted", function(e) {
-    if ($(e.target).attr('id') === 'js-pool-end') {
-      return initIssueCards();
+  $(document).bind("DOMNodeInserted", function(e) {
+    var target;
+    target = $(e.target);
+    if (target.attr('id') === 'js-pool-end') {
+      initIssueCards();
     }
+    if (target.attr('class') === 'external-link') {
+      target.attr('target', 'blank');
+    }
+    return true;
   });
+  return setTimeout((function() {
+    return $('#ghx-work').on('dblclick', '.ghx-issue', function(e) {
+      console.log("Clicked on ", $(e.currentTarget).find('.js-detailview')[0]);
+      return $(e.currentTarget).find('.js-detailview')[0].click();
+    });
+  }), 100);
 };
 
 script = document.createElement('script');
